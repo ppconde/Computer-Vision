7bc06e9f7f48b6bf47aa744c249203ef56945a8c
agsaraiva@ua.pt <agsaraiva@ua.pt>
2016-09-22 22:01:28 +0100

    2nd exercise included

--- Aula1/source/DisplayImage.cpp
+++ Aula1/source/DisplayImage.cpp
@@ -1,28 +1,37 @@
-#include <stdio.h>
-#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
 
 using namespace cv;
+using namespace std;
 
-int main(int argc, char** argv )
-{
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
+int main(int argc, char** argv){
+	//if the number of arguments given by terminal is not 1 (1+1=2), aborts
+	if (argc != 2){
+		cout << " Usage: LoadnDisplay <image path>" << endl;
+		return -1;
+	}
 
-    Mat image;
-    image = imread( argv[1], 1 );
+	//creates new "Mat" object to store image data
+	Mat image;
 
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
+	//initializes "image" by reading and loading image file data
+	image = imread(argv[1], CV_LOAD_IMAGE_COLOR);	//loads color data
 
-    waitKey(0);
+	//if no data given to image, aborts
+	if (!image.data){
+		cout << "Could not open or find the image." << endl;
+		return -1;
+	}
 
-    return 0;
+	//creates a window for image display
+	namedWindow("Display Window", CV_WINDOW_AUTOSIZE);
+
+	//presents image on window
+	imshow("Display Window", image);
+
+	//waits for keystroke in the window
+	waitKey(0);
+
+	return 0;
 }
