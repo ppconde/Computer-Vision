511ddb3d552f6a9cde30454b3ed705d90a24f858
ppconde <pepconde.1993@gmail.com>
2016-10-05 02:21:50 +0100

    Lecture03Report

---
+++ Aula2/src/CMakeFiles/ChangeBrigtCront.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChangeBrigtCront.dir/ChangeBrigtCront.cpp.o"
+  "ChangeBrigtCront.pdb"
+  "ChangeBrigtCront"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChangeBrigtCront.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
