067ce7f5646e9796257861a953d12d34794e4619
agsaraiva@ua.pt <agsaraiva@ua.pt>
2016-10-12 12:19:42 +0100

    GradCalc included

--- Aula4/src/GradCalc.cpp
+++ Aula4/src/GradCalc.cpp
@@ -5,34 +5,82 @@ using namespace cv;
 using namespace std;
 
 int main() {
+	char* windowName;
+	int type;
+	int ksize;
+	int scale = 1;
+	int delta = 0;
+	int ddepth = CV_16S;
+
+	//opens default camera
+	VideoCapture cap(0);
+	
+	//check if success
+	if (!cap.isOpened()) return -1;
+
+	//presents options to user
+	system("clear");		//clears terminal window
+	cout << endl << "---------------------" << endl;
+	cout << " Gradient Calculator " << endl;
+	cout << "---------------------" << endl << endl;
+	cout << " Choose image gradient calculation type (sobel = 0, sharr = 1, laplacian = 2: ";
+	cin >> type;
+
+	if (type != 1) {
+		cout << " Choose Sobel kernel size [1, 3, 5 or 7]: ";
+		cin >> ksize;
+	}
+
+	//creates horz and vert convolution matrixes
+	Mat gradX, gradY;
+	Mat absGradX, absGradY;
+
+	Mat grayscale, final;
+
+	namedWindow("Video Capture", 1);
+
 	for(;;) {
 		Mat frame;
 		cap >> frame;								//gets a new frame from camera
 		imshow("Video Capture", frame);				//shows original capture
 
+		//applies gaussian blur
+		GaussianBlur(frame, frame, Size(3, 3), 0, 0, BORDER_DEFAULT);
+
+		//converts to grayscale
 	    cvtColor(frame, grayscale, CV_BGR2GRAY);	//converts to grayscale
 	    
-	    if (!type) imshow("Filtered Capture", grayscale);	//if grayscale selected, display
-	    else {
-	    	//outputs selected threshold
-	    	if (method == 1) {
-	    		threshold(grayscale, bw, 0, 255, THRESH_BINARY + THRESH_OTSU);
-	    	}
-	    	else if (method == 2) {
-	    		adaptiveThreshold(grayscale, bw, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, threshValue, 0);
-	    	}
-	    	else if (method == 3) {
-	    		adaptiveThreshold(grayscale, bw, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, threshValue, 0);
-	    	}
-	    	else threshold(grayscale, bw, threshValue, 255, THRESH_BINARY);
+		if (type == 1) {
+			//calculates gradients using Scharr
+			Scharr(grayscale, gradX, ddepth, 1, 0, scale, delta, BORDER_DEFAULT);
+			Scharr(grayscale, gradY, ddepth, 0, 1, scale, delta, BORDER_DEFAULT);
 
-	    	imshow("Filtered Capture", bw);			//displays black & white image
+			windowName = "Scharr";
+		}
+		else if (type == 2) {
+			Laplacian(grayscale, final, ddepth, ksize, scale, delta, BORDER_DEFAULT);
+			convertScaleAbs(final, final);
+
+			windowName = "Laplacian";
+		}	    
+		else {
+			//calculates gradients using Sobel
+			Sobel(grayscale, gradX, ddepth, 1, 0, ksize, scale, delta, BORDER_DEFAULT);
+			Sobel(grayscale, gradY, ddepth, 0, 1, ksize, scale, delta, BORDER_DEFAULT);
+
+			windowName = "Sobel";
 		}
 
+		if (type == 0 || type == 1) {
+			convertScaleAbs(gradX, absGradX);
+			convertScaleAbs(gradY, absGradY);
+			addWeighted(absGradX, 0.5, absGradY, 0.5, 0, final);
+		}
+
+	    imshow(windowName, final);
+	    
 	    if (waitKey(5) >= 0) break;				//waits 30ms for program to render next frame
 	}
 
-	waitKey(0);
-	
 	return 0;
 }
