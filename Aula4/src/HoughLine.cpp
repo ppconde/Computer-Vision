63acc439a0339e6fb4c5d0f0f386255e7a4db462
ppconde <pepconde.1993@gmail.com>
2016-10-18 14:17:56 +0100

    Word and new files

--- Aula4/src/HoughLine.cpp
+++ Aula4/src/HoughLine.cpp
@@ -29,12 +29,20 @@ int main(int argc, char** argv)
  Canny(src, dst, 100, 500, 3);
  cvtColor(dst, cdst, CV_GRAY2BGR);
 
-  vector<Vec4i> lines;
-  HoughLinesP(dst, lines, 1, CV_PI/180, 150, 300, 100 );
+  vector<Vec2f> lines;
+  HoughLines(dst, lines, 1, CV_PI/180, 180, 500, 500 );
+
   for( size_t i = 0; i < lines.size(); i++ )
   {
-    Vec4i l = lines[i];
-    line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
+    float rho = lines[i][0], theta = lines[i][1];
+    Point pt1, pt2;
+    double a = cos(theta), b = sin(theta);
+    double x0 = a*rho, y0 = b*rho;
+    pt1.x = cvRound(x0 + 1000*(-b));
+    pt1.y = cvRound(y0 + 1000*(a));
+    pt2.x = cvRound(x0 - 1000*(-b));
+    pt2.y = cvRound(y0 - 1000*(a));
+    line( cdst, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
   }
 
  imshow("source", src);
