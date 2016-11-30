1f8ad7c5f4dfeabf91b9df152a43ab375517d629
ppconde <pepconde.1993@gmail.com>
2016-11-30 12:30:08 +0000

    Project Mouse handler

--- Project/src/Project.cpp
+++ Project/src/Project.cpp
@@ -4,16 +4,23 @@
 using namespace std;
 using namespace cv;
 
+void mouseHandler(int event, int x, int y, int flags, void* param);
+
+
+vector<Point2f> prevPts, nextPts;		//frame features
+vector<uchar> status;					//status vector
+vector<float> err;						//error vector
+
+int nPts = 500;						//number of features
+double qLevel = 0.5;				//quality level
+double minDist = 0.05;				//min euclidian distance
+
+int drag = 0, select_flag = 0;
+Mat frame, prevFrame, nextFrame;		//image matrixes
+Point point1, point2;
+bool callback = false;
+
 int main() {
-	Mat frame, prevFrame, nextFrame;		//image matrixes
-
-	vector<Point2f> prevPts, nextPts;		//frame features
-	vector<uchar> status;					//status vector
-	vector<float> err;						//error vector
-
-	int nPts = 500;						//number of features
-	double qLevel = 0.5;				//quality level
-	double minDist = 0.05;				//min euclidian distance
 
   //video file
 	VideoCapture cap;
@@ -29,7 +36,7 @@ int main() {
 	cout << "  Optical Flow with Lucas-Kanade " << endl;
 	cout << " --------------------------------" << endl << endl;
 
-	namedWindow("Optical Flow L-K", 1);
+	namedWindow("Optical Flow L-K", CV_WINDOW_AUTOSIZE);
 
 	//gets initial image
 	cap >> frame;
@@ -56,9 +63,38 @@ int main() {
 		}
 
 		imshow("Optical Flow L-K", frame);	//shows original capture
-
-	    if (waitKey(100) >= 0) break;				//waits 5ms for program to render next frame
+		setMouseCallback("frame",mouseHandler,0);
+		if(!frame.data) break;
+	    if (waitKey(50) >= 0) break;				//waits 5ms for program to render next frame
 	}
 
 	return 0;
+}
+void mouseHandler(int event, int x, int y, int flags, void* param)
+{
+    if (event == CV_EVENT_LBUTTONDOWN && !drag && !select_flag)
+    {
+        /* left button clicked. ROI selection begins */
+        point1 = Point(x, y);
+        drag = 1;
+    }
+
+    if (event == CV_EVENT_MOUSEMOVE && drag && !select_flag)
+    {
+        /* mouse dragged. ROI being selected */
+        Mat img1 = frame.clone();
+        point2 = Point(x, y);
+        rectangle(img1, point1, point2, CV_RGB(255, 0, 0), 3, 8, 0);
+        imshow(frame, img1);
+    }
+
+    if (event == CV_EVENT_LBUTTONUP && drag && !select_flag)
+    {
+        Mat img2 = frame.clone();
+        point2 = Point(x, y);
+        drag = 0;
+        select_flag = 1;
+        imshow(frame, img2);
+        callback = true;
+    }
 }
