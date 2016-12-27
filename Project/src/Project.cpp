80b1d2f431ccd6dbeebaab69757cffd6e8b1ea2e
agsaraiva@ua.pt <agsaraiva@ua.pt>
2016-12-27 22:46:22 +0000

    Project now has Farneback mode too

--- Project/src/Project.cpp
+++ Project/src/Project.cpp
@@ -4,13 +4,16 @@
 using namespace std;
 using namespace cv;
 
+#define STEP 		10
+#define FLOWSZ 		0.5
+#define FB_SCALE	10
 
 Mat roiFrame, roiAux;
 Rect roiBox;
 vector<Point2f> roiPts;		//points defining a ROI
-int cnt = 0;				//mouse clicks counter
+unsigned int cnt = 0;				//mouse clicks counter
 
-int funcInt;	//for program function type
+unsigned int funcInt;	//for program function type
 
 
 static void roiSelection(int event, int x, int y, int, void*) {
@@ -63,15 +66,18 @@ static void roiSelection(int event, int x, int y, int, void*) {
 
 int main() {
 	int sel;							//video selection
-	bool func;							//program function type
+	int func;							//program function type
 	char file[20];
 	int linesz;							//line thickness
 
-	Mat frame, prevFrame, nextFrame, roi;
+	Mat frame, prevFrame, nextFrame, roi, flow;
+	UMat flowUMat;
 
 	vector<Point2f> prevPts, nextPts;	//frame features
 	vector<uchar> status;				//status vector
 	vector<float> err;					//error vector
+
+	Point2f flowPt;
 
 	int nPts = 2000;					//number of features
 	double qLevel = 0.05;				//quality level
@@ -83,24 +89,36 @@ int main() {
 	//presents user interface
 	system("clear");		//clears terminal window
 	cout << endl
-		 << " --------------------------" << endl
-		 << "  Optical Flow for Project " << endl
-		 << " --------------------------" << endl << endl
+		 << " --------------------------------" << endl
+		 << "  Muscle Movement Quantification " << endl
+		 << " --------------------------------" << endl << endl
 		 << " Choose video file [1 - ?]: ";
 	cin >> sel;
 	cout << endl;
-	cout << " Choose [0 = ROI, 1 = single point]: ";
+	cout << " Choose [0 = ROI (L-K), 1 = single point (L-K), 2 = Grid (FB)]: ";
 	cin >> func;
-	cout << endl << endl;
+	cout << endl;
 
 	//properties for each program function
-	if (func) {
-		funcInt = 3;
-		linesz = 2;
-	}
-	else {
+	if (func == 0) {
 		funcInt = 2;
 		linesz = 1;
+
+		cout
+		<< " Select two points to define ROI with the mouse. A third mouse click will reset the selection." << endl
+		<< " Press ENTER when the selection is made." << endl << endl;
+
+	}
+	else if (func == 1) {
+		funcInt = 3;
+		linesz = 2;
+
+		cout
+		<< " Select one point of interest with the mouse. A second mouse click will reset the selection." << endl
+		<< " Press ENTER when the selection is made." << endl << endl;
+	}
+	else if (func == 2) {
+		cout << " Farneback selected" << endl << endl;
 	}
 
 	//video file
@@ -111,18 +129,17 @@ int main() {
 	cap.open(file);
 
 	//check if success
-	if (!cap.isOpened()) return -1;
+	if (!cap.isOpened()) {
+		cout << " Error: video file could not be loaded. Aborting." << endl << endl;
+		return -1;
+	}
 
 	for (;;) {
-        //displays point selection (cnt == 0 by default)
+		//initial behavior
 		if (cnt == 0) {
-			cout
-			<< " Select two points to define ROI with the mouse. A third mouse click will reset the selection." << endl
-			<< " Press ENTER when the selection is made." << endl << endl;
-
 			cap >> frame;		//gets new frame
 			if(!frame.data) {
-				cout << " Error: no video data found." << endl
+				cout << " Error: no video data found." << endl;
 				break;
 			}
 
@@ -132,41 +149,50 @@ int main() {
 			//converts frame to grayscale
 			cvtColor(frame, nextFrame, CV_BGR2GRAY);
 
-			//images for selection
-			roiFrame = frame.clone();
-			roiAux = roiFrame.clone();
+			if (func == 0 || func == 1) {
+				//images for selection
+				roiFrame = frame.clone();
+				roiAux = roiFrame.clone();
 
-			//mouse callback for selecting ROI
-			imshow("Optical Flow", roiFrame);
-			setMouseCallback("Optical Flow", roiSelection);
-			waitKey(0);
+				//mouse callback for selecting ROI
+				imshow("Optical Flow", roiFrame);
+				setMouseCallback("Optical Flow", roiSelection);
+				waitKey(0);
 
-			//if points have been selected and ROI defined
-			if (roiPts.size() == funcInt) {
-				//creates ROI and ROI mask
-				roi = frame(roiBox);
-				cvtColor(roi, nextFrame, CV_BGR2GRAY);
+				//if points have been selected and ROI defined
+				if (roiPts.size() == funcInt) {
+					//creates ROI and ROI mask
+					roi = frame(roiBox);
+					cvtColor(roi, nextFrame, CV_BGR2GRAY);
 
-				if (func) {
-					nextPts.push_back(roiPts[2]);
-					linPts.push_back(roiPts[2]);
+					if (func) {
+						nextPts.push_back(roiPts[2]);
+						linPts.push_back(roiPts[2]);
+					}
+				}
+				else {
+					cout << " Error: not enough points selected to form ROI." << endl;
+					return -1;
 				}
 			}
 			else {
-				cout << " Error: not enough points selected to form ROI." << endl;
-				return -1;
+				prevFrame = nextFrame.clone();
+
+				//overrides counter
+				cnt = 1;
 			}
 		}
 
-		//gets image features
-		if (!func) {
+		if (func == 0) {
+			//gets image features
 			goodFeaturesToTrack(nextFrame, nextPts, nPts, qLevel, minDist);
 		}
 
+		if (func == 0 || func == 1) prevPts = nextPts;
+
 		prevFrame = nextFrame.clone();		//first frame is the same as last one
-		prevPts = nextPts;
 		cap >> frame;						//gets a new frame from camera
-		
+			
 		//ends tracking if video ends
 		if(!frame.data) {
 			cout << " Video has ended. Tracking Stopped." << endl << endl;
@@ -176,53 +202,76 @@ int main() {
 		resize(frame, frame, Size(), 1.5, 1.5, INTER_CUBIC);
 		cvtColor(frame, nextFrame, CV_BGR2GRAY);
 
-		nextFrame = nextFrame(roiBox);
+		if (func == 0 || func == 1) {
+			nextFrame = nextFrame(roiBox);
 
-		//calculates optical flow using Lucas-Kanade
-		calcOpticalFlowPyrLK(prevFrame, nextFrame, prevPts, nextPts, status, err);
+			//calculates optical flow using Lucas-Kanade
+			calcOpticalFlowPyrLK(prevFrame, nextFrame, prevPts, nextPts, status, err);
 
-		if (!func) {
-			//draws ROI rectangle
-			rectangle(frame, roiPts[0], roiPts[1], Scalar(255, 255, 0), 1);
-		
+			if (func == 0) {
+				//draws ROI rectangle
+				rectangle(frame, roiPts[0], roiPts[1], Scalar(255, 255, 0), 1);
+				
+				//draws motion lines on display
+				for (unsigned int i = 0; i < nextPts.size(); i++) {
+					if (status[i]) {
+						x1 = roiPts[0].x + prevPts[i].x;
+						y1 = roiPts[0].y + prevPts[i].y;
+						x2 = roiPts[0].x + nextPts[i].x;
+						y2 = roiPts[0].y + nextPts[i].y;
 
-			//draws motion lines on display
-			for (int i = 0; i < nextPts.size(); i++) {
-				if (status[i]) {
-					x1 = roiPts[0].x + prevPts[i].x;
-					y1 = roiPts[0].y + prevPts[i].y;
-					x2 = roiPts[0].x + nextPts[i].x;
-					y2 = roiPts[0].y + nextPts[i].y;
+						/*cout << "PONTOS" << endl;
+						cout << roiPts << endl;
+						cout << prevPts[i] << endl;
+						cout << nextPts[i] << endl << endl;
 
-					/*cout << "PONTOS" << endl;
-					cout << roiPts << endl;
-					cout << prevPts[i] << endl;
-					cout << nextPts[i] << endl << endl;
+						cout << x1 << endl;
+						cout << y1 << endl;
+						cout << x2 << endl;
+						cout << y2 << endl << endl;*/
 
-					cout << x1 << endl;
-					cout << y1 << endl;
-					cout << x2 << endl;
-					cout << y2 << endl << endl;*/
+						line(frame, Point(x1, y1), Point(x2, y2), Scalar(255, 255, 0), linesz);
+					}
+				}
+			}
+			else {
+				//stores points in a new vector
+				linPts.push_back(nextPts[0]);
+				//cout << linPts << endl;
+				//cout << linPts.size() << endl << endl;
 
-					line(frame, Point(x1, y1), Point(x2, y2), Scalar(255, 255, 0), linesz);
+				//draws the path of selected point
+				for (unsigned int i = 0; i < linPts.size() - 1; i++) {
+					if (status[0]) {
+						//line(frame, linPts[i], linPts[i+1], Scalar(255, 255, 0), linesz);
+							
+						if (i == linPts.size() - 2) {
+							//draws at current point position
+							line(frame, linPts[i], linPts[i+1], Scalar(0, 0, 255), linesz);
+							//circle(frame, linPts[i+1], 5, Scalar(0, 0, 255), 1);
+						}
+						else {
+							line(frame, linPts[i], linPts[i+1], Scalar(255, 255, 0), linesz);
+						}
+					}
 				}
 			}
 		}
 		else {
-			//stores points in a new vector
-			linPts.push_back(nextPts[0]);
-			//cout << linPts << endl;
-			//cout << linPts.size() << endl << endl;
+			//calculates optical flow using Farneback
+			calcOpticalFlowFarneback(prevFrame, nextFrame, flowUMat, 0.5, 3, 15, 3, 5, 1.2, 0);
 
-			//draws the path of selected point
-			for (int i = 0; i < linPts.size() - 1; i++) {
-				if (status[0]) {
-					line(frame, linPts[i], linPts[i+1], Scalar(255, 255, 0), linesz);
-					
-					//draws circle at current point position
-					if (i == linPts.size() - 2) {
-						circle(frame, linPts[i+1], 5, Scalar(0, 0, 255), 1);
+			flowUMat.copyTo(flow);
+
+			//creates flow analysis grid
+			for (int i = 0; i < frame.rows; i += STEP) {
+				for (int j = 0; j < frame.cols; j += STEP) {
+					flowPt = flow.at<Point2f>(i, j)*FB_SCALE;
+
+					if ((flowPt.x > FLOWSZ || flowPt.x < -FLOWSZ) && (flowPt.y > FLOWSZ || flowPt.y < -FLOWSZ)) {
+						line(frame, Point(j, i), Point(j + flowPt.x, i + flowPt.y), Scalar(255, 255, 0));
 					}
+					else continue;
 				}
 			}
 		}
@@ -235,6 +284,7 @@ int main() {
 		//if "space" pressed, pauses. If "esc" pressed, exits program
 	    if (key == 32) {
 	    	key = 0;
+
 	    	while (key != 32) {
 	    		imshow("Optical Flow", frame);
 	    		key = waitKey(5);
